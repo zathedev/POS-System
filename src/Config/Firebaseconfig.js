20724a973fe50edc5568c4f3c8de058cfa954ecc
Abdullahawan00119 <abdullahawan00119@gmail.com>
2026-02-21 15:02:34 -0800

    Firebase env configuration

--- src/Config/Firebaseconfig.js
+++ src/Config/Firebaseconfig.js
@@ -10,6 +10,8 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
